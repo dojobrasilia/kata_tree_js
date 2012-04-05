bcdf76db9957444caeb0483d48e55cf5a6b59278
Bruno Rolim <brolim@gmail.com>
2012-04-05 19:18:57 -0300

    criação de nós. Criamos os objetos e o html

--- spec/tree_spec.js
+++ spec/tree_spec.js
@@ -1,13 +1,65 @@
 describe("Tree", function() {
 
-	describe("Node", function() {
+  describe("Node", function() {
 
-    it("creates a node with its name from JSON", function() {
-      var json = {name:'node'};
-      var node = new Node(json);
-      expect(node instanceof Node).toBe(true);
-      expect(node.name).toBe('node');
+    describe("object creation", function() {
+
+    	it("creates a node from JSON", function () {
+        var json = {name: 'node'};
+        var node  = new Node(json)
+
+        expect(node.name).toBe('node');
+        expect(node instanceof Node).toBe(true);
+      });
+
+      it("creates a node with a child from JSON", function () {
+        var json = {name: 'node', children:[{name:'child'}]};
+        var node  = new Node(json)
+
+        expect(node.children[0].name).toBe('child');
+        expect(node.children[0] instanceof Node).toBe(true);
+      });
+
+      it("creates a node with two children from JSON", function () {
+        var json = {name: 'node', children:[{name:'child1'}, {name:'child2'}]};
+        var node  = new Node(json)
+
+        expect(node.children[0].name).toBe('child1');
+        expect(node.children[0] instanceof Node).toBe(true);
+
+        expect(node.children[1].name).toBe('child2');
+        expect(node.children[1] instanceof Node).toBe(true);
+      });
+
+      it("creates a node with one grand_child from JSON", function () {
+        var json = {name: 'node', children:[{name:'child1', children:[{name:'grand_child'}]}]};
+        var node  = new Node(json)
+
+        var grand_child = node.children[0].children[0];
+        expect(grand_child.name).toBe('grand_child');
+        expect(grand_child instanceof Node).toBe(true);
+      });
+
+    });
+
+    describe("html creation", function() {
+
+      it("creates a div with node's name when a Node is created", function () {
+        var json = {name: 'node', children:[{name:'child1', children:[{name:'grand_child'}]}]};
+        var node  = new Node(json);
+
+        expect($('<p></p>').append(node.html).html()).toContain('<div class="node">node');
+      });
+
+      it("creates a div for each node", function () {
+        var json = {name: 'node', children:[{name:'child1', children:[{name:'grand_child'}]}]};
+        var node  = new Node(json);
+
+        expect($('<p></p>').append(node.html).find('.node').length).toBe(3);
+      });
+
     });
 
   });
+
 });
--- src/tree.js
+++ src/tree.js
@@ -1,3 +1,18 @@
-Node = function(data) {
-	this.name = data.name;
+Node = function (data) {
+  this.name = data.name;
+  this.html = $('\
+    <div class="node">'
+      + this.name +
+    '</div>'
+  );
+
+  var self = this;
+
+  this.children = [];
+  if(data.children)
+    data.children.forEach(function (child) {
+      var new_node = new Node(child);
+      self.children.push(new_node);
+      self.html.append(new_node.html);
+    });
 };
